eab02002312220dc275e23bd52d68f860570c459
Paulo Kositis <paulokositis@gmail.com>
2026-03-22 04:41:51 -0300

    Estudo de bots atualizados, comando slash, tree e embeds estudados

--- Bot 1/assets/main.py
+++ Bot 1/assets/main.py
@@ -5,13 +5,14 @@ from dotenv import load_dotenv
 from pathlib import Path
 import datetime, pytz
 
-
+#definimos o fusohorario e o horário para o bot mandar algo especifico
 dt = datetime.datetime.now(tz=pytz.timezone('America/Sao_Paulo'))
 fuso_horario = dt.tzinfo
 horario = datetime.time(20, 3, tzinfo=fuso_horario)
 
 intents = discord.Intents.all()
 
+#aqui dizemos que todo commando do bot vai ser com .
 bot = commands.Bot(".", intents=intents)
 
 env_path = Path(__file__).parent / ".env"
@@ -30,12 +31,12 @@ except:
 
 
 
-
+#Aqui inicia o bot e dps ele manda uma mensagem falando q iniciou
 @bot.event
 async def on_ready():
     enviar_mensagem.start()
     print("Bot inicializado bitch")
-
+#ESSA FUNÇÃO FAZ COM QUE O BOT MANDE UMA MENSAGEM TODA VEZ Q VC DIGITAR .OLA
 @bot.command()
 async def ola(ctx):
     await ctx.reply("Ola! Tudo bem?")
@@ -45,12 +46,12 @@ async def namorar(ctx:commands.Context):
     nome = ctx.author.name
     await ctx.reply(f"Namorar com você? {nome} você quer ser humilhado agora ou depois? pobre imundo")
 
-@bot.event
-async def on_message(msg:discord.Message):
-    if msg.author.bot:
-        return
-    await msg.reply(f"O {msg.author.mention} Enviou uma mensagem no canal {msg.channel.name}")
-    await bot.process_commands(msg)
+# @bot.event
+# async def on_message(msg:discord.Message):
+#     if msg.author.bot:
+#         return
+#     await msg.reply(f"O {msg.author.mention} Enviou uma mensagem no canal {msg.channel.name}")
+#     await bot.process_commands(msg)
 
 @bot.event
 async def on_member_join(member:discord.Member):
@@ -70,6 +71,24 @@ async def somar(ctx:commands.Context, num1:int, num2:int):
 async def enviar_mensagem():
     canal = bot.get_channel(1415696880138195036)  
     await canal.send(f"Oi, por acaso sua mãe sabe q vc gosta de rapazes?")
+
+@bot.command()
+async def enviar_embed(ctx:commands.Context):
+    minha_embed = discord.Embed()
+    minha_embed.title = "Titulo mt criativo pra quem n tem nada"
+    minha_embed.description = "Minha descrição superior"
+
+
+    caminho_base = os.path.dirname(__file__ )
+    caminho_imagem = os.path.join(caminho_base, "img", "72.jpg")
+    imagem = discord.File(caminho_imagem, "Safada.jpg")
+
+    minha_embed.set_image(url="attachment://Safada.jpg" )
+
+    await ctx.reply(embed=minha_embed, file=imagem)
+
+
+
          
 
 
