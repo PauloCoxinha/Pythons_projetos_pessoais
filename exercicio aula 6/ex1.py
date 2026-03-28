9bdef8808932fb6c3e4ffb0d4c915b7dc789eccd
Paulo Kositis <paulokositis@gmail.com>
2026-03-27 23:48:52 -0300

    NAO VCOU PERDER A DROGGGA DA STREAK

---
+++ exercicio aula 6/ex1.py
@@ -0,0 +1,23 @@
+escolha = input("Você quer a temperatura de Fahrenheit ou Celsius? Escreva F para Fahrenheit e C para celsius: ")
+
+escolha_maiuscula = escolha.upper()
+    
+
+numero = int(input("Escreva uma temperatura (ela vai interpretada como Celsius, porém ela vai ser convertida caso você escolheu Fahrenheit): "))
+
+
+
+def Fahrenheit():
+    conversaoFah = numero * 1.8 + 30
+
+    return conversaoFah
+
+
+if escolha_maiuscula == 'C':
+    print(f"{numero}°c")
+
+elif escolha_maiuscula == 'F':
+    print(f"{Fahrenheit():.2f}°F")
+
+else:
+    print("Você deve estar se achando o sabichão por pular as instruções né")
