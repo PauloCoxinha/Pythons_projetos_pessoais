8531b55528ab1d33cd38757c3647e0e079ccfd88
Paulo Kositis <paulokositis@gmail.com>
2026-04-20 22:40:41 -0300

    Estudos em json

---
+++ json/main.py
@@ -0,0 +1,55 @@
+import json 
+
+json_string = """{
+
+"personas": [ 
+{
+"nome": "João",
+"idade": 22,
+"cidade": "São_Paulo",
+"profissao": "Engenheiro"
+},
+{
+"nome": "João",
+"idade": 22,
+"cidade": "São_Paulo",
+"profissao": "padeiro"
+},
+{
+"nome": "João",
+"idade": 22,
+"cidade": "São_Paulo",
+"profissao": "metaleiro"
+}
+]}"""
+
+dados = json.loads(json_string)
+
+
+for pessoa in dados['personas']:
+    nome = pessoa['nome']
+    idade = pessoa['idade']
+    cidade = pessoa['cidade']
+    profissao = pessoa['profissao']
+
+    print(f"\n nome: {nome} \n idade: {idade} \n cidade: {cidade} \n profissao: {profissao} ")
+
+
+cachorro = {
+    "raça": 'Husk',
+    "idade": 2,
+    "dono": "Paulo",
+    "país": "Canadá"
+}
+
+json_cachorro = json.dumps(cachorro)
+
+print(type(json_cachorro))
+print(type(dados))
+
+
+
+
+with open('frutas.json', 'r', encoding='utf-8') as arq:
+    dadosreais = json.load(arq)
+    print(dadosreais)
