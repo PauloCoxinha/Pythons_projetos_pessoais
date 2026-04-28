1503c68509cb195822da3e8e3f2b25e74aaa96ae
Paulo Kositis <paulokositis@gmail.com>
2026-04-27 23:38:40 -0300

    Creio que eu não atualizei sobre o flask ainda

--- FLET/aula1/main.py
+++ FLET/aula1/main.py
@@ -3,14 +3,27 @@ from flet import Checkbox, FloatingActionButton, Icons, Page, TextField
 
 def main(page: ft.Page):
     def add_clicked(e):
-
-        page.add(Checkbox(label=new_task.value))
+        task_view.controls.append(Checkbox(label=new_task.value))
         new_task.value = ''
-        print(e)
+        print(e)        
         page.update()
 
-    new_task = TextField(hint_text="O que você quer fazer?")
-    page.add(new_task, FloatingActionButton(icon=Icons.ADD, on_click=add_clicked))
+    new_task = TextField(hint_text="O que você quer fazer?", expand=True)
+    task_view = ft.Column()
+    view = ft.Column(
+        width=350,  
+        controls=[
+                ft.Row(
+                    controls=[
+                        new_task,
+                        ft.FloatingActionButton(icon=Icons.ADD, on_click=add_clicked),    
+                    ],
+                ),
+                task_view,
+        ],
+    )
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
 
     page.add(ft.Text(value="Seja bem vindo a tela mais legal dos animes"))
     pass
